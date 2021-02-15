cb2e6823c568b549b34788a9436b19d5f7b7bb0c
maciel <makjesus2008@gmail.com>
2021-02-15 19:45:45 -0300

    Atualizar

--- seqCres.php
+++ seqCres.php
@@ -1,28 +1,23 @@
 
-<!-- Receba como parametro um array de números inteiros e responda TRUE or FALSE se é possível obter uma sequencia crescente removendo apenas um elemento do array.
+<?php
 
-Exemplos para teste 
 
-Obs.:-  É Importante  realizar todos os testes abaixo para garantir o funcionamento correto.
-         -  Sequencias com apenas um elemento são consideradas como TRUE -->
+function seqCres($array){
+    
+     sort($array);
+    
+    if($array == $array - 1){
+        return true;
+    }
+    
+    if($array != $array){
+        return false;
+    }
+}
+    
+   
 
-<!-- [1, 3, 2, 1]  false
-[1, 3, 2]  true
-[1, 2, 1, 2]  false
-[3, 6, 5, 8, 10, 20, 15] false
-[1, 1, 2, 3, 4, 4] false
-[1, 4, 10, 4, 2] false
-[10, 1, 2, 3, 4, 5] true
-[1, 1, 1, 2, 3] false
-[0, -2, 5, 6] true
-[1, 2, 3, 4, 5, 3, 5, 6] false
-[40, 50, 60, 10, 20, 30] false
-[1, 1] true
-[1, 2, 5, 3, 5] true
-[1, 2, 5, 5, 5] false
-[10, 1, 2, 3, 4, 5, 6, 1] false
-[1, 2, 3, 4, 3, 6] true
-[1, 2, 3, 4, 99, 5, 6] true
-[123, -17, -5, 1, 2, 3, 12, 43, 45] true
-[3, 5, 67, 98, 3] true          -->
-
+echo seqCres(1,3,4,6,7,8,5,6);
+ 
+?>       
+ 
